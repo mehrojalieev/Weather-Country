c8370f7436b9caf05cf7e1df2e0fed863fb7065e
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-02-27 23:24:11 +0500

    Bug fixed

--- Js/app.js
+++ Js/app.js
@@ -1,55 +1,74 @@
-const Form = document.querySelector("#search-form")
-const SearchInput = document.querySelector("#search-input")
 const CurrentWeatherBackground = document.querySelector(".weather__output-wrapper")
+const HourlyWeatherCards = document.querySelector("#hourly__weather-container")
 const CurrentWeatherOutput = document.querySelector("#current__weather-output")
-const WeatherInfoDay = document.querySelector("#weather__info-day")
+const DailyWeatherCards = document.querySelector("#daily__weather-container")
 const CurrentCountryName = document.querySelector("#current__country-name")
 const CurrentWeatherIcon = document.querySelector("#current__weather-icon")
 const CurrentDescription = document.querySelector("#current-description")
-const CurrentFeels = document.querySelector("#current-feels")
-const CurrentDrop = document.querySelector("#current-drop")
 const CurrentVisibility = document.querySelector("#current-visibility")
 const CurrentHumidity = document.querySelector("#current-humidity")
+const WeatherInfoDay = document.querySelector("#weather__info-day")
 const HumidityPoint = document.querySelector("#humidity-point")
-
-const HourlyWeatherCards = document.querySelector("#hourly__weather-container")
-const DailyWeatherCards = document.querySelector("#daily__weather-container")
+const CurrentFeels = document.querySelector("#current-feels")
+const SearchInput = document.querySelector("#search-input")
+const CurrentDrop = document.querySelector("#current-drop")
 const UviIndex = document.querySelector(".uvi-index")
+const Form = document.querySelector("#search-form")
+
 
 
 const API_KEY = "644f6ce0ca9e401ebb891832211707";
 
 
 
-const CloudyImages = ['https://wp-s.ru/wallpapers/13/9/356138679766943/milye-oblaka-plyvut-po-nebu.jpg', 'https://catherineasquithgallery.com/uploads/posts/2021-02/1612751479_207-p-goluboi-fon-s-oblakami-dlya-fotoshopa-254.jpg', 'https://ak.picdn.net/shutterstock/videos/1056283103/thumb/1.jpg']
-const ClearImages = ['https://w.forfun.com/fetch/ac/acf0ba2588cdf5635ee80a045cf89b90.jpeg', 'https://catherineasquithgallery.com/uploads/posts/2021-02/1612767398_123-p-fon-goluboe-nebo-163.jpg', 'https://images.hdqwalls.com/download/sky-minimal-2m-3840x2160.jpg', 'https://klike.net/uploads/posts/2023-04/1681702375_2-2.jpg']
-const SmokeImages = ['https://avatars.mds.yandex.net/i?id=c9cdc92cfabf1765a4ac62d0abc156e4_l-4966461-images-thumbs&ref=rim&n=13&w=1080&h=1131', 'https://avatars.mds.yandex.net/i?id=da668686b370a43a29e0e5a32c1c28d3_l-4704243-images-thumbs&ref=rim&n=13&w=1080&h=720', 'https://avatars.mds.yandex.net/i?id=993a9b7b2cb9b9a463efd4e6c1b306a2_l-4900601-images-thumbs&ref=rim&n=13&w=1080&h=1080']
+const CloudyImages = [
+    'https://wp-s.ru/wallpapers/13/9/356138679766943/milye-oblaka-plyvut-po-nebu.jpg',
+    'https://catherineasquithgallery.com/uploads/posts/2021-02/1612751479_207-p-goluboi-fon-s-oblakami-dlya-fotoshopa-254.jpg',
+    'https://ak.picdn.net/shutterstock/videos/1056283103/thumb/1.jpg'
+]
+const ClearImages = [
+    'https://w.forfun.com/fetch/ac/acf0ba2588cdf5635ee80a045cf89b90.jpeg',
+    'https://catherineasquithgallery.com/uploads/posts/2021-02/1612767398_123-p-fon-goluboe-nebo-163.jpg',
+    'https://images.hdqwalls.com/download/sky-minimal-2m-3840x2160.jpg', 'https://klike.net/uploads/posts/2023-04/1681702375_2-2.jpg'
+]
+const SmokeImages = [
+    'https://avatars.mds.yandex.net/i?id=c9cdc92cfabf1765a4ac62d0abc156e4_l-4966461-images-thumbs&ref=rim&n=13&w=1080&h=1131',
+    'https://avatars.mds.yandex.net/i?id=da668686b370a43a29e0e5a32c1c28d3_l-4704243-images-thumbs&ref=rim&n=13&w=1080&h=720',
+    'https://avatars.mds.yandex.net/i?id=993a9b7b2cb9b9a463efd4e6c1b306a2_l-4900601-images-thumbs&ref=rim&n=13&w=1080&h=1080'
+]
+const SnowImages = [
+    'https://mir-s3-cdn-cf.behance.net/project_modules/1400/9b481488117677.5dcc715a9112e.jpg'
+]
 
-    function RandomCloudyImages() {
-        var randomIndex = Math.floor(Math.random() * CloudyImages.length)
-        return CloudyImages[randomIndex]
-    }
+function RandomCloudyImages() {
+    var randomIndex = Math.floor(Math.random() * CloudyImages.length)
+    return CloudyImages[randomIndex]
+}
 
-    function RandomClearImages() {
-        var randomIndex = Math.floor(Math.random() * ClearImages.length)
-        return ClearImages[randomIndex]
-    }
-    function RandomSmokeImages() {
-        var randomIndex = Math.floor(Math.random() * SmokeImages.length)
-        return SmokeImages[randomIndex]
-    }
+function RandomClearImages() {
+    var randomIndex = Math.floor(Math.random() * ClearImages.length)
+    return ClearImages[randomIndex]
+}
+function RandomSmokeImages() {
+    var randomIndex = Math.floor(Math.random() * SmokeImages.length)
+    return SmokeImages[randomIndex]
+}
+function RandomSnowImages() {
+    var randomIndex = Math.floor(Math.random() * SnowImages.length)
+    return SnowImages[randomIndex]
+}
 
-    
-    
-    Form.addEventListener('submit', Weatherdata)
-    
-    async function Weatherdata(e) {
-        e.preventDefault( )
+
+
+Form.addEventListener('submit', Weatherdata)
+
+async function Weatherdata(e) {
+    e.preventDefault()
     try {
 
         let response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${SearchInput.value ? SearchInput.value : "Tashkent"}&appid=9dd86907fe501cec50da3d087e4e9dc0`);
         let WeatherLocation = await response.json()
-        const  Lat = WeatherLocation[0]?.lat
+        const Lat = WeatherLocation[0]?.lat
         const Lon = WeatherLocation[0]?.lon;
         console.log("lat", Lat, "lon", Lon);
 
@@ -75,37 +94,40 @@ function renderWeatherData(WeatherLocation, WeatherData, weatherIcon) {
     console.log(WeatherData);
 
     CurrentCountryName.innerHTML = WeatherLocation[0].name
-    CurrentWeatherOutput.innerHTML = WeatherData.current.temp+'°'
+    CurrentWeatherOutput.innerHTML = WeatherData.current.temp + '°'
     WeatherInfoDay.innerHTML = WeatherData.current.weather[0].main
-    if(WeatherData.current.weather[0].main === "Clouds"){
-        CurrentWeatherBackground.style = `background-image: url(${RandomCloudyImages()})`
+    if (WeatherData.current.weather[0].main === "Clouds") {
+        document.body.style = `background-image: url(${RandomCloudyImages()})`
     }
-    else if(WeatherData.current.weather[0].main === "Clear"){
-        CurrentWeatherBackground.style = `background-image: url(${RandomClearImages()})`
+    else if (WeatherData.current.weather[0].main === "Clear") {
+        document.body.style = `background-image: url(${RandomClearImages()})`
     }
-    else if(WeatherData.current.weather[0].main === "Smoke"){
-        CurrentWeatherBackground.style = `background-image: url(${RandomSmokeImages()})`
+    else if (WeatherData.current.weather[0].main === "Smoke") {
+        document.body.style = `background-image: url(${RandomSmokeImages()})`
+    }
+    else if (WeatherData.current.weather[0].main === "Snow") {
+        document.body.style = `background-image: url(${RandomSnowImages()})`
     }
     CurrentWeatherIcon.src = weatherIcon.url
     CurrentDescription.innerHTML = WeatherData.daily[0].summary
-    CurrentFeels.innerHTML = WeatherData.current.feels_like+'°'
+    CurrentFeels.innerHTML = WeatherData.current.feels_like + '°'
     CurrentDrop.innerHTML = WeatherData.current.dew_point
-    CurrentVisibility.innerHTML = WeatherData.current.visibility+' mi'
-    CurrentHumidity.innerHTML = WeatherData.current.humidity+'%'
+    CurrentVisibility.innerHTML = WeatherData.current.visibility + ' mi'
+    CurrentHumidity.innerHTML = WeatherData.current.humidity + '%'
     UviIndex.innerHTML = WeatherData.current.uvi
     HumidityPoint.innerHTML = `The dew point is ${WeatherData.current.dew_point} right now`
-    HourlyWeatherCards.innerHTML = ''  
+    HourlyWeatherCards.innerHTML = ''
     WeatherData.hourly.forEach(hour => {
         const hourData = new Date(hour.dt * 1000).getHours()
-        
+
         const Div = document.createElement('div')
-            Div.innerHTML = `
-                <p> ${hourData+':00'}</p>
+        Div.innerHTML = `
+                <p> ${hourData + ':00'}</p>
                 <h4>${hour.temp}°</h4>
                 <img src=${weatherIcon.url}>
-            `   
-            HourlyFragment.appendChild(Div)
-            HourlyWeatherCards.appendChild(HourlyFragment)
+            `
+        HourlyFragment.appendChild(Div)
+        HourlyWeatherCards.appendChild(HourlyFragment)
     }
     );
 
@@ -118,7 +140,7 @@ function renderWeatherData(WeatherLocation, WeatherData, weatherIcon) {
         var weekdayName = weekdays[dayOfWeek];
         console.log(weekdayName);
         Div.innerHTML = `
-        <p>${weekdays.slice(0,3)}</p>
+        <p>${weekdays.slice(0, 3)}</p>
         <h4>${daily.temp.day}°</h4>
         <img src=${weatherIcon.url}>
         `
